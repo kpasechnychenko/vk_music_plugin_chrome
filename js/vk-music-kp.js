277cf4595d167dc7f4bbcef73c99071bd27f5689
Eugene Glova <eugene.glova@gmail.com>
2014-02-25 14:24:40 +0200

    Optimize js

--- js/vk-music-kp.js
+++ js/vk-music-kp.js
@@ -16,6 +16,9 @@ function show_hide_elm(selector) {
 
 
 function bind_events() {
+    var ac_vol = $("#ac_vol"),
+        ac_pr = $("#ac_pr");
+
     $("#ac_play").on('click', function () {
         console.log("play");
     });
@@ -40,54 +43,54 @@ function bind_events() {
         console.log("similar songs");
     });
 
-    $("#ac_pr").on('mouseenter', function () {
+    ac_pr.on('mouseenter', function () {
         console.log("mouseenter");
         $(this).addClass("over");
     });
 
-    $("#ac_pr").on('mouseleave', function () {
+    ac_pr.on('mouseleave', function () {
         console.log("mouseleave");
         $(this).removeClass("over");
     });
 
-    $("#ac_pr").on('mousedown', function (e) {
+    ac_pr.on('mousedown', function (e) {
         console.log("mousedown");
         $(this).addClass("down");
         $(this).find("#ac_pr_line").width(e.offsetX);
     });
 
-    $("#ac_pr").on('mouseup', function () {
+    ac_pr.on('mouseup', function () {
         console.log("mouseup");
         $(this).removeClass("down");
     });
 
-    $("#ac_pr").on('mousemove', function (e) {
+    ac_pr.on('mousemove', function (e) {
         if ($(this).hasClass("down") && e.pageX - e.offsetX == 90) {
             $(this).find("#ac_pr_line").width(e.offsetX);
         }
     });
 
-    $("#ac_vol").on('mouseenter', function () {
+    ac_vol.on('mouseenter', function () {
         console.log("mouseenter");
         $(this).addClass("over");
     });
 
-    $("#ac_vol").on('mouseleave', function () {
+    ac_vol.on('mouseleave', function () {
         console.log("mouseleave");
         $(this).removeClass("over");
     });
 
-    $("#ac_vol").on('mousedown', function () {
+    ac_vol.on('mousedown', function () {
         console.log("mousedown");
         $(this).addClass("down");
     });
 
-    $("#ac_vol").on('mouseup', function () {
+    ac_vol.on('mouseup', function () {
         console.log("mouseup");
         $(this).removeClass("down");
     });
 
-    $("#ac_vol").on('mousemove', function (e) {
+    ac_vol.on('mousemove', function (e) {
         console.log(e.pageX - e.offsetX);
         if ($(this).hasClass("down") && e.pageX - e.offsetX == 456) {
             $(this).find("#ac_vol_line").width(e.offsetX);
